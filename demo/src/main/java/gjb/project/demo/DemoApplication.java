eed380852bd4bd9b0f56fc95d1fbbfdb2fd1a092
G631233828 <631233828@qq.com>
2019-08-14 17:15:54 +0800

    版本提交

--- demo/src/main/java/gjb/project/demo/DemoApplication.java
+++ demo/src/main/java/gjb/project/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//测试提交
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
